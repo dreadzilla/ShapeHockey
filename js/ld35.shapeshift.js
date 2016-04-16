fdd0661d41f707f13cb81e5fbc97b1412c054bc9
dreadzilla <hakan.staby@gmail.com>
2016-04-16 11:41:24 +0200

    First
    Starting up

---
+++ js/ld35.shapeshift.js
@@ -0,0 +1,13 @@
+/*
+	Shapeshift theme game made for Ludum Dare 35
+	Created by Håkan Ståby
+*/
+
+// a global object to hold all global variables related to the game.
+var shapeshiftGame = {};
+
+shapeshiftGame.savingObject = {};
+
+// store the counting elapsed time.
+shapeshiftGame.savingObject.currentElapsedTime = 0;
+
