d280b37f3e5edc514d72522318262a1f6bdd59d5
dreadzilla <hakan.staby@gmail.com>
2016-04-16 14:56:49 +0200

    Added phaser
    Added phaser

--- js/ld35.shapeshift.js
+++ js/ld35.shapeshift.js
@@ -1,6 +1,6 @@
 /*
 	Shapeshift theme game made for Ludum Dare 35
-	Created by Håkan Ståby
+	Created by Hakan Staby
 */
 
 // a global object to hold all global variables related to the game.
@@ -10,4 +10,24 @@ shapeshiftGame.savingObject = {};
 
 // store the counting elapsed time.
 shapeshiftGame.savingObject.currentElapsedTime = 0;
+
+$(function(){	
+
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+	
+
+});
+
+function preload () {
+
+            game.load.image('logo', 'images/phaser.png');
+
+        }
+
+        function create () {
+
+            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+            logo.anchor.setTo(0.5, 0.5);
+
+        }
 
